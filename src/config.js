10a84c8b54fdaa48670ef925b15e4f239c1e885d
monocrowd <monocrowd@gmail.com>
2021-11-28 12:10:13 +0800

    change config

--- src/config.js
+++ src/config.js
@@ -5,14 +5,6 @@ function getConfig(env) {
 
   case 'production':
   case 'mainnet':
-    return {
-      networkId: 'mainnet',
-      nodeUrl: 'https://rpc.mainnet.near.org',
-      contractName: CONTRACT_NAME,
-      walletUrl: 'https://wallet.near.org',
-      helperUrl: 'https://helper.mainnet.near.org',
-      explorerUrl: 'https://explorer.mainnet.near.org',
-    }
   case 'development':
   case 'testnet':
     return {
