a8c7d7c2eec5c00a0415230eae3a8797542de4e3
monocrowd <monocrowd@gmail.com>
2021-11-28 10:36:22 +0800

    basic version

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
   // use React Hooks to store greeting in component state
-  const [greeting, set_greeting] = React.useState()
+  const [candidates, set_candidates] = React.useState([])
 
   // when the user has not yet interacted with the form, disable the button
-  const [buttonDisabled, setButtonDisabled] = React.useState(true)
-
-  // after submitting the form, we want to show Notification
-  const [showNotification, setShowNotification] = React.useState(false)
+  const [buttonDisabled, setButtonDisabled] = React.useState(false)
 
   // The useEffect hook can be used to fire side-effects during render
   // Learn more: https://reactjs.org/docs/hooks-intro.html
@@ -24,9 +21,9 @@ export default function App() {
       if (window.walletConnection.isSignedIn()) {
 
         // window.contract is set by initContract in index.js
-        window.contract.get_greeting({ account_id: window.accountId })
-          .then(greetingFromContract => {
-            set_greeting(greetingFromContract)
+        window.contract.view_candidates({ accountId: window.accountId })
+          .then(result => {
+            set_candidates(result)
           })
       }
     },
@@ -41,17 +38,7 @@ export default function App() {
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
-        <h1>Welcome to NEAR!</h1>
-        <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
-          below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
-        </p>
+        <h1>Welcome to NEARVEMBER Challenge 8!</h1>
         <p>
           Go ahead and click the button below to try it out:
         </p>
@@ -76,53 +63,41 @@ export default function App() {
               color: 'var(--secondary)',
               borderBottom: '2px solid var(--secondary)'
             }}
-          >
-            {greeting}
+          > It is time to vote
           </label>
           {' '/* React trims whitespace around tags; insert literal space character when needed */}
           {window.accountId}!
         </h1>
+        <CandidateList candidates={candidates} />
         <form onSubmit={async event => {
           event.preventDefault()
 
-          // get elements from the form using their id attribute
-          const { fieldset, greeting } = event.target.elements
+          // // get elements from the form using their id attribute
+          const { fieldset, newCandidateInput } = event.target.elements
 
-          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-          const newGreeting = greeting.value
+          // // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
+          const newCandidate = newCandidateInput.value
 
-          // disable the form while the value gets updated on-chain
+          // // disable the form while the value gets updated on-chain
           fieldset.disabled = true
 
           try {
             // make an update call to the smart contract
-            await window.contract.set_greeting({
+            await window.contract.add_candidate({
               // pass the value that the user entered in the greeting field
-              message: newGreeting
+              candidate_id: newCandidate
             })
           } catch (e) {
-            alert(
-              'Something went wrong! ' +
-              'Maybe you need to sign out and back in? ' +
-              'Check your browser console for more info.'
-            )
-            throw e
+            alert(e)
           } finally {
             // re-enable the form, whether the call succeeded or failed
             fieldset.disabled = false
           }
 
-          // update local `greeting` variable to match persisted value
-          set_greeting(newGreeting)
-
-          // show Notification
-          setShowNotification(true)
-
-          // remove Notification again after css animation completes
-          // this allows it to be shown again next time the form is submitted
-          setTimeout(() => {
-            setShowNotification(false)
-          }, 11000)
+          window.contract.view_candidates({ accountId: window.accountId })
+            .then(result => {
+              set_candidates(result)
+            })
         }}>
           <fieldset id="fieldset">
             <label
@@ -133,65 +108,71 @@ export default function App() {
                 marginBottom: '0.5em'
               }}
             >
-              Change greeting
+              Nominate new candidate
             </label>
             <div style={{ display: 'flex' }}>
               <input
                 autoComplete="off"
-                defaultValue={greeting}
-                id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
+                defaultValue="someone"
+                placeholder="candidate"
+                id="newCandidateInput"
                 style={{ flex: 1 }}
               />
               <button
                 disabled={buttonDisabled}
                 style={{ borderRadius: '0 5px 5px 0' }}
               >
-                Save
+                Submit
               </button>
             </div>
           </fieldset>
         </form>
-        <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
-        </p>
-        <ol>
-          <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>get_greeting</code> and <code>set_greeting</code> being called on <code>contract</code>. What's this?
-          </li>
-          <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/develop/contracts/overview">smart contract</a>.</li>
-          <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
-        </ol>
-        <hr />
-        <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
-        </p>
       </main>
-      {showNotification && <Notification />}
     </>
   )
 }
 
-// this component gets rendered by App after the form is submitted
-function Notification() {
-  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
+function CandidateList({ candidates }) {
+  const [buttonDisabled, setButtonDisabled] = React.useState(false)
+
+  const onClick = async (id) => {
+    console.log(id)
+    setButtonDisabled(true)
+    try {
+      // make an update call to the smart contract
+      await window.contract.vote({
+        // pass the value that the user entered in the greeting field
+        candidate_id: id
+      })
+    } catch (e) {
+      alert(e)
+    } finally {
+      // re-enable the form, whether the call succeeded or failed
+      setButtonDisabled(false)
+    }
+  }
+
   return (
-    <aside>
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
-        {window.accountId}
-      </a>
-      {' '/* React trims whitespace around tags; insert literal space character when needed */}
-      called method: 'set_greeting' in contract:
-      {' '}
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
-        {window.contract.contractId}
-      </a>
-      <footer>
-        <div>✔ Succeeded</div>
-        <div>Just now</div>
-      </footer>
-    </aside>
+    <div>
+      <h2>Candidates</h2>
+      <ul>{candidates.map(c => (
+        <li style={{ display: 'flex' }} key={c.candidate_id}>
+          <div style={{ display: 'flex' }}>
+            <input
+              autoComplete="off"
+              defaultValue={c.candidate_id}
+              style={{ flex: 1 }}
+            />
+            <button
+              style={{ borderRadius: '0 5px 5px 0' }}
+              onClick={() => onClick(c.candidate_id)}
+              disabled={buttonDisabled}
+            >
+              Vote
+            </button>
+          </div>
+        </li>
+      ))}</ul>
+    </div>
   )
 }
